4c3d7977906a799d502a2de2e3893567b02d4f9c
gingus55 <gingus55@hotmail.com>
2021-10-02 08:00:38 +0100

    added my question array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
 console.log("Hello world");
 
+// I need an array of questions defined globally
+const questionArray = [
+    {question: "What acronym is used for 'Application Program Interface'?",
+     answer: 'API',
+     decoy: ['IBM', "CSS", "ABC"]},
+    {question: "A Boolean can return 'true' and ...?",
+     answer: "false",
+     decoy: ["say what?", "wrong", "whatever"]},
+    {question: "A 'string' is an example of an ...",
+     answer: "data type",
+     decoy: ["toy", "fruit", "API"]},
+    {question: "Who is the best Avenger?",
+     answer: "Hulk",
+     decoy: ["Iron Man", "Thor", "Black Widow"]},
+    {question: "How many primitive data types are there?",
+     answer: "7",
+     decoy: ["5","8","6"]},
+    {question: "Which of the following is an example of a 'loop'?",
+     answer: "while",
+     decoy: ["whenever","meanwhile","justify"]},
+    {question: "What is the acronym for Hypertext Markup Language?",
+     answer: "HTML",
+     decoy: ["CSS", "JS", "HML"]},
+    {question: "Which of the following asks if something is strictly equal?",
+     answer: "===",
+     decoy: ["==", "!==", "="]},
+    {question: "What does CSS stand for?",
+     answer: "Cascading style sheets",
+     decoy: ["Cascading sheet styles", "Computer styling sheets", "Computational sheet styles"]},
+    {question: "Which is the index value of the first element in an array?",
+     answer: "0",
+     decoy: ["1", "first", "foo"]}       
+];
+
+
 // i need to make the start button commence the quiz
 const startClick = document.querySelector("#start");
 const mainScreen = document.getElementById("main-container");
