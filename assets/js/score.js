ad9a2d100455c69ca6cc71f6122da2ed4612b371
gingus55 <gingus55@hotmail.com>
2021-10-03 18:13:46 +0100

    added scores

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,7 +1,17 @@
 const scoreList = document.getElementById("scoreList");
 const scores = JSON.parse(localStorage.getItem("highScores")) || [];
+const results = document.createElement("ul");
 
 console.log("Hello world");
 console.log(scores);
 
-scoreList;
+for (let i = 0; i < scores.length; i++) {
+  const list = document.createElement("li");
+  list.setAttribute("class", "lists");
+  list.textContent = scores[i].scorer + " scored " + scores[i].score;
+
+  results.appendChild(list);
+}
+
+const final = document.getElementById("scoreList");
+final.appendChild(results);
