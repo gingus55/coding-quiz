fe62f0a6a75eaecfe6360af902e3a116b891a5b7
gingus55 <gingus55@hotmail.com>
2021-10-03 13:36:25 +0100

    tidied js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,60 @@
-console.log("Hello world");
-
 // I need an array of questions defined globally
 const questionArray = [
-    {"question": "What acronym is used for 'Application Program Interface'?",
-     "answer": 'API',
-     "decoy": ['IBM', "CSS", "ABC","API"]},
-    {"question": "A Boolean can return 'true' and ...?",
-     "answer": "false",
-     "decoy": ["say what?", "wrong", "false", "whatever"]},
-    {"question": "A 'string' is an example of an ...",
-     "answer": "data type",
-     "decoy": ["toy", "data type", "fruit", "API"]},
-    {"question": "Who is the best Avenger?",
-     "answer": "Hulk",
-     "decoy": ["Iron Man", "Hulk", "Thor", "Black Widow"]},
-    {"question": "How many primitive data types are there?",
-     "answer": "7",
-     "decoy": ["5", "8", "6", "7"]},
-    {"question": "Which of the following is an example of a 'loop'?",
-     "answer": "while",
-     "decoy": ["whenever","meanwhile","justify", "while"]},
-    {"question": "What is the acronym for Hypertext Markup Language?",
-     "answer": "HTML",
-     "decoy": ["CSS", "JS", "HTML", "HML"]},
-    {"question": "Which of the following asks if something is strictly equal?",
-     "answer": "===",
-     "decoy": ["==","===" , "!==", "="]},
-    {"question": "What does CSS stand for?",
-     "answer": "Cascading style sheets",
-     "decoy": ["Cascading sheet styles", "Cascading style sheets", "Computer styling sheets", "Computational sheet styles"]},
-    {"question": "Which is the index value of the first element in an array?",
-     "answer": "0",
-     "decoy": ["1", "first", "foo", "0"]}       
+  {
+    question: "What acronym is used for 'Application Program Interface'?",
+    answer: "API",
+    decoy: ["IBM", "CSS", "ABC", "API"],
+  },
+  {
+    question: "A Boolean can return 'true' and ...?",
+    answer: "false",
+    decoy: ["say what?", "wrong", "false", "whatever"],
+  },
+  {
+    question: "A 'string' is an example of an ...",
+    answer: "data type",
+    decoy: ["toy", "data type", "fruit", "API"],
+  },
+  {
+    question: "Who is the best Avenger?",
+    answer: "Hulk",
+    decoy: ["Iron Man", "Hulk", "Thor", "Black Widow"],
+  },
+  {
+    question: "How many primitive data types are there?",
+    answer: "7",
+    decoy: ["5", "8", "6", "7"],
+  },
+  {
+    question: "Which of the following is an example of a 'loop'?",
+    answer: "while",
+    decoy: ["whenever", "meanwhile", "justify", "while"],
+  },
+  {
+    question: "What is the acronym for Hypertext Markup Language?",
+    answer: "HTML",
+    decoy: ["CSS", "JS", "HTML", "HML"],
+  },
+  {
+    question: "Which of the following asks if something is strictly equal?",
+    answer: "===",
+    decoy: ["==", "===", "!==", "="],
+  },
+  {
+    question: "What does CSS stand for?",
+    answer: "Cascading style sheets",
+    decoy: [
+      "Cascading sheet styles",
+      "Cascading style sheets",
+      "Computer styling sheets",
+      "Computational sheet styles",
+    ],
+  },
+  {
+    question: "Which is the index value of the first element in an array?",
+    answer: "0",
+    decoy: ["1", "first", "foo", "0"],
+  },
 ];
 
 // this is my main container
@@ -44,31 +67,28 @@ const starterPage = document.querySelector("#starting-page");
 // I need a timer to be present
 let timeRemaining = 75;
 
-const startTimer = function(){
-    const countdownElement = document.querySelector("#countdown");
-    // declare tick function
-    const timerTick=function(){
-        timeRemaining -= 1;
-        countdownElement.textContent = timeRemaining;
-    };
-
-    // set interval
-    const timer = setInterval(timerTick, 1000);
-}
-
-const begin = function(){
-//   needs to remove starting page
-starterPage.remove(); 
-// needs to render questions
-buildQuestionPage();
-
-console.log("clicked!");
-// needs to start timer
-startTimer();
-
+const startTimer = function () {
+  const countdownElement = document.querySelector("#countdown");
+  // declare tick function
+  const timerTick = function () {
+    timeRemaining -= 1;
+    countdownElement.textContent = timeRemaining;
+  };
+  // set interval
+  const timer = setInterval(timerTick, 1000);
 };
 
-startClick.addEventListener('click', begin);
+const begin = function () {
+  //   needs to remove starting page
+  starterPage.remove();
+  // needs to render questions
+  buildQuestionPage();
+
+  // needs to start timer
+  startTimer();
+};
+
+startClick.addEventListener("click", begin);
 
 let questionNumber = 0;
 
@@ -114,21 +134,17 @@ const questionPage = document.querySelector("#question-page");
 const answerSelection = document.querySelector("#main-container");
 
 const handleAnswer = function (event) {
-  const currentTarget = event.currentTarget;
   const target = event.target;
 
-  //   console.log(currentTarget);
-  //   console.log(target);
   const correctAnswer =
     target.getAttribute("id") === target.getAttribute("data-log");
 
   if (correctAnswer) {
-    console.log("correct");
     document.getElementById("main-container").innerHTML = "";
 
     buildQuestionPage();
   } else {
-    alert("GAME OVER!!!!");
+    timeRemaining -= 5;
   }
 };
 
