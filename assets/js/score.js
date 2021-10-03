dc58819bf27f672ee495e9b97cfff12a67d946f1
gingus55 <gingus55@hotmail.com>
2021-10-03 18:33:12 +0100

    reset scores removes list

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,9 +2,6 @@ const scoreList = document.getElementById("scoreList");
 const scores = JSON.parse(localStorage.getItem("highScores")) || [];
 const results = document.createElement("ul");
 
-console.log("Hello world");
-console.log(scores);
-
 for (let i = 0; i < scores.length; i++) {
   const list = document.createElement("li");
   list.setAttribute("class", "lists");
@@ -15,3 +12,14 @@ for (let i = 0; i < scores.length; i++) {
 
 const final = document.getElementById("scoreList");
 final.appendChild(results);
+
+const resetBtn = document.querySelector("#reset");
+
+const toStart = function () {
+  location.href = "./index.html";
+};
+
+const reset = function () {
+  localStorage.clear();
+  results.remove();
+};
